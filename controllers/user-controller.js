0617bccb8813c9f648a6002236a90a369df45259
allan.pirillis <allan.p94@gmail.com>
2021-07-05 12:13:32 -0500

    created the model schemas and associations; now working on the controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,48 @@
+const { User } = require("../models");
+
+const userController = {
+  getAllUsers(req, res) {
+    User.find({})
+      .populate(
+        {
+          path: "thoughts",
+          select: "-__v",
+        },
+        {
+          path: "friends",
+          select: "-__v",
+        }
+      )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate(
+        {
+          path: "thoughts",
+          select: "-__v",
+        },
+        {
+          path: "friends",
+          select: "-__v",
+        }
+      )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ messaage: "No user found with this id" });
+          return;
+        }
+
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+};
